e99e745b3fc98f06e600e16c04c3693be4081b75
Amiya kiran <amiya.susy@gmail.com>
2023-03-17 12:31:13 +0530

    color change

--- script.js
+++ script.js
@@ -19,20 +19,30 @@ for(let i=0; i<totalNavList; i++){
     const a = navList[i].querySelector("a");
    
     a.addEventListener("click",function(){
-        for(let i=0; i<totalSection; i++){
-            allSection[i].classList.remove("back-section");
-        }
+        removeBackSection();
 
         for(let j=0; j<totalNavList; j++){
             if(navList[j].querySelector("a").classList.contains("active")){
+
+                addBackSection(j);
                 /* console.log("back-section" + navList[j].querySelector("a")); */
-                allSection[j].classList.add("back-section");
+               /*  allSection[j].classList.add("back-section"); */
+
             }
             navList[j].querySelector("a").classList.remove("active");
         }
         this.classList.add("active")
         showSection(this);
     })
+}
+function removeBackSection(){
+    for(let i=0; i<totalSection; i++){
+        allSection[i].classList.remove("back-section");
+    }
+}
+
+function addBackSection(num){
+    allSection[num].classList.add("back-section");
 }
 function showSection(element){
 
@@ -43,6 +53,24 @@ function showSection(element){
    console.log(target);
    document.querySelector("#" + target).classList.add("active");
 }
+function updateNav(element){
+    for(let i=0; i<totalSection; i++){
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+    /* console.log( element.getAttribute("href").split("#")[1]); */
+}
+document.querySelector(".hire-me").addEventListener("click", function(){
+    const sectionIndex = this.getAttribute("data-section-index");
+     console.log(sectionIndex); 
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
 
 //for side navbar to open
 const navTogglerBtn = document.querySelector(".nav-toggle"),
